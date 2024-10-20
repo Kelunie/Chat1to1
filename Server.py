a972ad2f707f83fca864ca6de3ec756f6e0c428e
Kelunie <caleb.cordero1997@gmail.com>
2024-10-19 20:53:25 -0600

    Ahora se hace conexion por IP introducida

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-import socket
+"""import socket
 import threading
 # headdecks
 
@@ -38,7 +38,44 @@ def runServer():
         # hile para manejar el cliente
         cliente_hilo = threading.Thread(target=handle_cliente, args=(cliente_socket, cliente_address))
         cliente_hilo.start()
+"""
+import socket
+
+# Configuramos el servidor
+host = '0.0.0.0'  # Esto escucha todas las interfaces
+port = 5000  # Puerto en el que escuchará el servidor
+
+# Creamos las variables para el socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Hacemos que se ligue a la red
+server_socket.bind((host, port))
+# Ahora que escuche las conexiones
+server_socket.listen(1)  # Escucha 1 conexión a la vez
+
+print(f"Esperando conexiones en {host}: {port} ...")
+
+# Aceptar una conexión
+conn, addr = server_socket.accept()
+print(f"Conectado por {addr}")
+
+# Recibir el nombre del cliente
+client_name = conn.recv(1024).decode("utf-8")
+print(f"Cliente conectado: {client_name}")
+
+while True:
+    data = conn.recv(1024).decode("utf-8")
+    if data == "exit":
+        print(f"Se cerro la conexión")
+        break
+    print(f"{client_name}: {data}")
+    reply = input("Tú: ")
+    conn.send(reply.encode("utf-8"))
+
+# Cerramos la conexión
+conn.close()
 
 
+"""
 if __name__ == '__main__':
     runServer()
+"""
--- cliente.py
+++ cliente.py
@@ -1,6 +1,7 @@
 import socket
 
 
+"""
 def run_cliente():
     # conectarnos al server
     cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,3 +25,33 @@ def run_cliente():
 
 if __name__ == '__main__':
     run_cliente()
+    
+"""
+
+# Datos del servidor (IP y Puerto)
+server_ip = input("Introduzca la IP del Servidor: ")
+port = 5000
+
+# Crear el socket
+cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Conectar al servidor
+cliente_socket.connect((server_ip, port))
+
+# Obtener el nombre del equipo
+nombre_equipo = socket.gethostname()
+
+# Enviar nombre del equipo al servidor
+cliente_socket.send(nombre_equipo.encode("utf-8"))
+
+# Bucle para enviar y recibir mensajes
+while True:
+    message = input(f"{nombre_equipo}: ")
+    cliente_socket.send(message.encode("utf-8"))
+
+    data = cliente_socket.recv(1024).decode("utf-8")
+    print(f"Servidor: {data}")
+
+    if message == "exit":
+        break
+
+cliente_socket.close()
